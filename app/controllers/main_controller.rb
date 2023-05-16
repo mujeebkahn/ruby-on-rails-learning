34d79cbf8927e9ed5489afd074ad5f02a59b9710
Mujeeb Ur Rehman <mujeebcahn@yahoo.com>
2023-05-17 03:48:43 +0500

    changes to push the code to repo

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,6 @@ class MainController < ApplicationController
     def index
         flash[:notice] = "Logged in Successfully"
         flash[:alert] = "Invalid Email or password"
+        flash[:crypt] = "Invalid Email or password"
     end
 end
