68eb72997abcf8e47cb9ce2ebb7d67b766940ae1
Mujeeb Ur Rehman <mujeebcahn@yahoo.com>
2023-05-17 03:38:13 +0500

    Initial commit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
     def index
-        
+        flash[:notice] = "Logged in Successfully"
+        flash[:alert] = "Invalid Email or password"
     end
 end
